bf87fc26519a87bd752201df42b33c6097b6fa41
KSPR <robot@kasparallenbach.ch>
2025-03-02 16:24:50 +0000

    copiloc cant add file

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/my-vue-app/' : '/',
+  publicPath: process.env.NODE_ENV === 'production',
   outputDir: 'dist',
   assetsDir: 'assets',
   lintOnSave: false,
